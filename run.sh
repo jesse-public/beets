bf60569a545d84ce08b0ef926b798a185bd1e9c3
Jesse <Jesse>
2025-04-10 19:54:25 -0700

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-beets import /downloads
-rm -r /downloads/*
+docker run \
+  --rm \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ \
+  -v ./volumes/beets/ingest:/downloads \
+  -v ./volumes/beets/import.sh:/import.sh \
+  -v /mnt/nas-music-flac:/music \
+  lscr.io/linuxserver/beets:latest bash /import.sh
--- volumes/beets/import.sh
+++ volumes/beets/import.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-docker run \
-  --rm \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ \
-  -v ./volumes/beets/ingest:/downloads \
-  -v ./volumes/beets/import.sh:/import.sh \
-  -v /mnt/nas-music-flac:/music \
-  lscr.io/linuxserver/beets:latest bash /import.sh
+beets import /downloads
+rm -r /downloads/*
